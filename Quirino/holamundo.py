31fcd59290e9e55839cc9daa8fd4535bfc794589
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-06-05 21:56:06 -0500

    -

---
+++ Quirino/holamundo.py
@@ -0,0 +1,3 @@
+print("hello world")
+name = "Juan Carlos Quirino"
+print(f"my name is {name}")
---
+++ Quirino/p1.py
@@ -0,0 +1,13 @@
+height = 5
+
+while height > 0:
+    print(height * "*")
+    height = height - 1
+
+'''
+*****
+****
+***
+**
+*
+'''
